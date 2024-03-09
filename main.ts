dfff78ed32fe0c33ea33d90de161ca90e8ad3632
colorchestra <colorchestra@users.noreply.github.com>
2024-03-09 10:38:34 +0100

    add oop

--- main.ts
+++ main.ts
@@ -1,7 +1,6 @@
 serial.redirectToUSB()
 let width = 16
 let height = 6
-//let rows = []
 let cols: boolean[][] = []
 let last_led_toggle = 0
 let player1_x = 3
@@ -9,6 +8,51 @@ let player1_y = 3
 let player1_direction_x = true
 let player1_direction_y = true
 
+class Player {
+    pos_x: number;
+    pos_y: number;
+    velocity_x: number;
+    velocity_y: number;
+    lit: boolean;
+    brightness: number;
+
+    constructor(pos_x: number, pos_y: number, brightness: number, lit: boolean) {
+        //this.pos_x = randint(0, width)
+        //this.pos_y = randint(0, height)
+        this.pos_x = pos_x
+        this.pos_y = pos_y
+        this.velocity_x = randint(1, 2)
+        this.velocity_y = randint(1, 2)
+        this.lit = false
+        this.brightness = brightness
+    }
+
+    detect_collision() {
+        // wall collision
+        if (this.pos_x <= 0 || this.pos_x >= width) {
+            this.velocity_x = this.velocity_x * -1
+        }
+        if (this.pos_y <= 0 || this.pos_y >= height) {
+            this.velocity_y = this.velocity_y * -1
+        }
+
+        // object collision
+        /*
+        if (this.lit != cols[this.pos_x][this.pos_y]){
+            this.velocity_x = this.velocity_x * -1
+            this.velocity_y = this.velocity_y * -1
+
+        }
+        */
+
+    }
+
+    move() {
+        this.pos_x += this.velocity_x
+        this.pos_y += this.velocity_y
+        this.detect_collision()
+    }
+}
 function winkekatze() {
     if (last_led_toggle + 1000 < control.millis()) {
         led.toggle(2, 0)
@@ -24,9 +68,12 @@ function render_grid() {
             }
         }
     }
-    scrollbit.setPixel(player1_x, player1_y, 128)
+    scrollbit.setPixel(player1.pos_x, player1.pos_y, player1.brightness)
+    scrollbit.setPixel(player2.pos_x, player2.pos_y, player2.brightness)
+
 }
 
+/*
 function move_player() {
     // x
     // move
@@ -62,6 +109,7 @@ function move_player() {
         player1_direction_y = !player1_direction_y
     }
 }
+*/
 
 // init array
 for (let x = 0; x <= scrollbit.cols(); x++) {
@@ -77,13 +125,18 @@ for (let x = 0; x <= scrollbit.cols(); x++) {
     }
 }
 
+// init player
+let player1 = new Player(3, 3, 128, true)
+let player2 = new Player(12, 3, 16, false)
 
 
 basic.forever(function on_forever() {
     winkekatze()
     
     scrollbit.clear()
-    move_player()
+    //move_player()
+    player1.move()
+    player2.move()
     render_grid()
     scrollbit.show()
     basic.pause(100)
